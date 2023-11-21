87ef94ed3993d4cea8e6d4aa262ea18d581c9712
caro3896 <70881644+caro3896@users.noreply.github.com>
2023-11-21 13:22:53 +0100

    middleware for accessing admin page

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,10 @@ Route::post('logout', [LoginController::class, 'destroy'])->middleware('auth')->
 Route::middleware('auth')->group(function () {
     // Route to main page/index
     Route::get('/', [MainController::class, 'index'])->name('index');
+
+    // Routes to admin pages - only acccessible if admin -> using middleware 'Admin'
+    Route::middleware('admin')->group(function () {
+        // Route to admin page
+        Route::get('admin', [MainController::class, 'admin'])->name('admin');
+    });
 });
