6288027849d46b6e279d3651af4e73220966a3c3
caro3896 <70881644+caro3896@users.noreply.github.com>
2023-11-24 14:00:31 +0100

    vote button changed

--- resources/js/Components/Rewards/Reward.vue
+++ resources/js/Components/Rewards/Reward.vue
@@ -55,15 +55,14 @@ export default {
     <div class="rounded-lg w-full relative">
         <img class="w-full h-full rounded-lg object-cover" :src="reward.image_url" alt="">
         <div v-if="isAvailable">
-            <div class="absolute top-0 left-0 flex justify-between p-3 w-full">
+            <div class="absolute top-0 left-0 flex justify-between p-3 w-full" @click="vote(reward)">
                 <div class="bg-gray rounded-2xl py-1 px-3 flex"> {{ reward.votes }} <img src="/icons/like.svg" class="ml-2" alt="Thumbs up icon"></div>
                 <p class="bg-yellow rounded-2xl py-1 px-3 text-gray">{{ reward.price }} x coins</p>
             </div>
             <div @click="open()" class="absolute bg-white w-full rounded-t-2xl rounded-b-xl bottom-0 left-0 p-6 max-h-18 overflow-hidden before:absolute before:h-1 before:w-20 before:bg-white-gray before:top-1.5 before:left-1/2 before:-translate-x-1/2 before:rounded-full cursor-pointer transition-max-height duration-1000 ease-in-out" :class="{ 'max-h-full': isOpen }">
               <h2 class="text-2xl mb-3 text-gray"> {{ reward.name }}</h2>
               <p class="mb-3 text-light-gray"> {{ reward.description }}</p>
-              <!-- <Button @click="buy(reward)">Indløs</Button> -->
-              <Button @click="vote(reward)">Stem</Button>
+              <Button @click="buy(reward)">Indløs</Button>
             </div>
         </div>
         <div v-else>
