d442f1ccf2e4289497b6e2f22171d06b9682cb35
caro3896 <70881644+caro3896@users.noreply.github.com>
2023-12-06 10:57:50 +0100

    resolve page component added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,12 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import '../css/app.css'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import route from "ziggy-js";
 
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
-      },
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     const app = createApp({ render: () => h(App, props) })
       .use(plugin)
